1de1d71a10887c92091d3509ab7ccf972e836be2
Carl Kakisis <Messinias1@gmail.com>
2019-06-16 13:15:08 -0400

    react and redux auth state added

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -20,6 +20,7 @@ export default function(state = initialState, action) {
                     status: null,
                     id: null
                 }
-                default: return state
+                default: 
+                    return state
     }
 }
