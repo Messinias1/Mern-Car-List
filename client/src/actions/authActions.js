5a83309068cb0ad6d07f9a0dcadd65aea4087040
Carl Kakisis <Messinias1@gmail.com>
2019-06-16 18:42:33 -0400

    created a register user modal

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,12 +4,12 @@ import { returnErrors } from './errorActions'
 import {
     USER_LOADED,
     USER_LOADING,
-    AUTH_ERROR
-    // LOGIN_SUCCESS,
-    // LOGIN_FAIL,
-    // LOGOUT_SUCCESS,
-    // REGISTER_SUCCESS,
-    // REGISTER_FAIL
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
 } from './types'
 
 // Check token and load user
@@ -29,6 +29,31 @@ export const loadUser = () => (dispatch, getState) => {
              })
          })
 
+}
+
+// Register User
+export const register = ({ name, email, password }) => dispatch => {
+    // Headers
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // Request body
+    const body = JSON.stringify({ name, email, password })
+
+    axios.post('/api/users', body, config)
+        .then(res => dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        }))
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'))
+            dispatch({
+                type: REGISTER_FAIL
+            })
+        })
 }
 
 // Setup config/headers and token
